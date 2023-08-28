a75a5d9438b6d835ae4a9b0d1d77eab866322a78
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-08-28 19:20:51 -0300

    Initial commit.

---
+++ src/main/kotlin/com/verge/theverge/TheVergeApplication.kt
@@ -0,0 +1,11 @@
+package com.verge.theverge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheVergeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheVergeApplication>(*args)
+}
---
+++ src/test/kotlin/com/verge/theverge/TheVergeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.verge.theverge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheVergeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
