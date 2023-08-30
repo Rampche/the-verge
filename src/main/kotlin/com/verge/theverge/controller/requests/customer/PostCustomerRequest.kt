29d6385bef48ef9c1ce5d65ff6811d6434d8d953
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-08-30 09:52:30 -0300

    Added "@fields" for spring validation.

--- src/main/kotlin/com/verge/theverge/controller/requests/customer/PostCustomerRequest.kt
+++ src/main/kotlin/com/verge/theverge/controller/requests/customer/PostCustomerRequest.kt
@@ -1,7 +1,14 @@
 package com.verge.theverge.controller.requests.customer
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+
 data class PostCustomerRequest(
+    @field:NotEmpty
     val name: String,
+
+    @field:Email
     val email: String,
+
     val password: String
 )
