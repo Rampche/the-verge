ab5141e0c616939d6cc49adff1f43bfdafd7e0d4
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-08-30 09:53:06 -0300

    Added "status" variable.

--- src/main/kotlin/com/verge/theverge/controller/requests/employee/PostEmployeeRequest.kt
+++ src/main/kotlin/com/verge/theverge/controller/requests/employee/PostEmployeeRequest.kt
@@ -1,10 +1,12 @@
 package com.verge.theverge.controller.requests.employee
 
+import com.verge.theverge.enums.EmployeeStatus
 import com.verge.theverge.enums.RoleType
 
 data class PostEmployeeRequest (
     val name: String,
     val email: String,
     val password: String,
-    val role: RoleType
+    val role: RoleType,
+    val status: EmployeeStatus
 )
