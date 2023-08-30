acda261d5f4fd55143d929c12aafc7a31705a0a9
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-08-30 09:56:11 -0300

    Updated errors enums messages to be more accurate.

--- src/main/kotlin/com/verge/theverge/enums/Errors.kt
+++ src/main/kotlin/com/verge/theverge/enums/Errors.kt
@@ -1,16 +1,36 @@
 package com.verge.theverge.enums
 
 enum class Errors(val code: String, val message: String) {
-    VG0001("VG-0001", "The table [%s] is already reserved"),
-    VG0002("VG-0002", "Customer nº [%s] not found"),
-    VG0003("VG-0003", "Table nº [%s] not found"),
-    VG0004("VG-0004", "Employee nº [%s] not found"),
-    VG0005("VG-0005", "Reservation nº [%s] not found"),
-    VG0006("VG-0006", "Order nº [%s] not found"),
-    VG0007("VG-0007", "[%s] employees not found"),
-    VG0008("VG-0008", "[%s] customers not found"),
-    VG0009("VG-0009", "Invoice nº [%s] not found"),
-    VG0010("VG-0010", "OrderItem nº [%s] not found"),
-    VG0011("VG-0011", "Item nº [%s] not found")
+
+    //Customers Exceptions
+    VG101("VG-101", "Customer nº [%s] not found"),
+    VG102("VG-102", "[%s] customers not found"),
+    VG103("VG-103", "Can't change customer which status is already [%s]"),
+    VG104("VG-104","Unable to update a consumer with [%s] status"),
+
+    //Tables Exceptions
+    VG201("VG-201", "The table [%s] is already reserved"),
+    VG202("VG-202", "Table nº [%s] not found"),
+
+    //Employees Exceptions
+    VG301("VG-301", "Employee nº [%s] not found"),
+    VG302("VG-302", "[%s] employees not found"),
+    VG303("VG-303", "Cant change employee [%s] status to [%s]"),
+    VG304("VG-304","Unable to update a employee with [%s] status"),
+
+    //Reservation Exceptions
+    VG401("VG-401", "Reservation nº [%s] not found"),
+
+    //Orders Exceptions
+    VG501("VG-501", "Order nº [%s] not found"),
+
+    //Invoices Exceptions
+    VG601("VG-601", "Invoice nº [%s] not found"),
+
+    //OrderItems Exceptions
+    VG701("VG-701", "OrderItem nº [%s] not found"),
+
+    //Items Exceptions
+    VG801("VG-801", "Item nº [%s] not found")
 
 }
