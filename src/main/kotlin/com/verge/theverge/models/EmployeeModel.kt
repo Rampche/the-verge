e76431407ea78c08d2d823c2e44cd62946062086
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-08-30 10:03:06 -0300

    Removed RoleType and Employee status definition by default.

--- src/main/kotlin/com/verge/theverge/models/EmployeeModel.kt
+++ src/main/kotlin/com/verge/theverge/models/EmployeeModel.kt
@@ -2,7 +2,9 @@ package com.verge.theverge.models
 
 
 import com.verge.theverge.enums.EmployeeStatus
+import com.verge.theverge.enums.Errors
 import com.verge.theverge.enums.RoleType
+import com.verge.theverge.exception.StatusException
 import jakarta.persistence.*
 
 @Entity(name = "employee")
@@ -20,12 +22,29 @@ data class EmployeeModel(
 
     @Column(name="role")
     @Enumerated(EnumType.STRING)
-    var role: RoleType? = RoleType.COMMON,
+    var role: RoleType,
 
     @Column(name = "password")
-    var password: String,
+    var password: String
 
+) {
     @Column(name="status")
     @Enumerated(EnumType.STRING)
-    var status: EmployeeStatus? = EmployeeStatus.ACTIVE,
-)
+    var status: EmployeeStatus ? = null
+        set(value) {
+            if (field == EmployeeStatus.DISMISSED){
+                throw StatusException(Errors.VG103.message.format(field), Errors.VG103.code)
+            }
+            field = value
+        }
+    constructor(
+        id: Int? = null,
+        name: String,
+        email: String,
+        role: RoleType,
+        password: String,
+        status: EmployeeStatus?
+    ): this(id, name, email, role, password){
+        this.status = status
+    }
+}
