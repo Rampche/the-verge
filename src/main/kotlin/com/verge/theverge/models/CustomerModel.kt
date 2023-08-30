30e0816290e1a3209102f35453e6acb33d135d61
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-08-30 09:58:57 -0300

    Updated to put a constructor, prevent changes to deleted customers and adjust status.

--- src/main/kotlin/com/verge/theverge/models/CustomerModel.kt
+++ src/main/kotlin/com/verge/theverge/models/CustomerModel.kt
@@ -2,6 +2,9 @@ package com.verge.theverge.models
 
 import com.verge.theverge.enums.CustomerStatus
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.verge.theverge.enums.Errors
+import com.verge.theverge.exception.BadRequestException
+import com.verge.theverge.exception.StatusException
 import jakarta.persistence.*
 
 @Entity(name = "customer")
@@ -19,13 +22,30 @@ data class CustomerModel (
 
     @JsonIgnore
     @Column(name= "password")
-    var password: String,
+    var password: String
 
+    //? = CustomerStatus.ACTIVE
+){
     @Column(name= "status")
     @Enumerated(EnumType.STRING)
-    var status: CustomerStatus? = CustomerStatus.ACTIVE
+    var status: CustomerStatus ? = null
+        set(value) {
+            if (field == CustomerStatus.DELETED){
+                throw BadRequestException(Errors.VG103.message.format(field), Errors.VG103.code)
+            }
+            field = value
+        }
 
-)
+    constructor(
+        id: Int? = null,
+        name: String,
+        email: String,
+        password: String,
+        status: CustomerStatus?
+    ): this(id, name, email, password){
+        this.status = status
+    }
+}
 
 //{
 //    constructor() : this(1, "Bruna", "bruna.teste@email.com", "123456", CustomerStatus.ACTIVE)
