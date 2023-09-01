d0786dd8b45bf81d99ad0bf9f75c1ac0f7f398cc
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-01 10:50:40 -0300

    Deleted invoice schema.

--- src/main/resources/db/migration/V11__create_invoice_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE invoice(
-invoice_id int auto_increment primary key,
-order_id int,
-FOREIGN KEY (order_id) REFERENCES orders(orders_id),
-orderitem_id int,
-FOREIGN KEY (orderitem_id) REFERENCES order_item(orderitem_id),
-total decimal(10,2)
-);
