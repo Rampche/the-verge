8a0ba18468c0c056f156a878dbf18e94fbc0b0a7
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-01 10:50:21 -0300

    Deleted invoice repo, controller, service and model.

--- src/main/kotlin/com/verge/theverge/repository/InvoiceRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.verge.theverge.repository
-
-
-import com.verge.theverge.models.InvoiceModel
-import org.springframework.data.repository.CrudRepository
-
-interface InvoiceRepository:CrudRepository<InvoiceModel, Int> {
-    
-}
