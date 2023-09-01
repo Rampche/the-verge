61438a8e1d0cf1ee92c973f3fc3d0de350b01261
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-01 10:52:45 -0300

    Removed unused import.

--- src/main/kotlin/com/verge/theverge/controller/OrderItemController.kt
+++ src/main/kotlin/com/verge/theverge/controller/OrderItemController.kt
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController
 class OrderItemController(
     val orderItemService: OrderItemService,
     val itemService: ItemsService,
-    val orderService: OrderService
 ) {
 
     @PostMapping
