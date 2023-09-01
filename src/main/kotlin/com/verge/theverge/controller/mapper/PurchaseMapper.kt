a4254f9727e87b26567380a229b624993e1cb071
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-01 11:02:01 -0300

    Creation of event listener to calculate the total amount of any purchase.

---
+++ src/main/kotlin/com/verge/theverge/controller/mapper/PurchaseMapper.kt
@@ -0,0 +1,26 @@
+package com.verge.theverge.controller.mapper
+
+import com.verge.theverge.controller.requests.PostPurchaseRequest
+import com.verge.theverge.models.PurchaseModel
+import com.verge.theverge.services.ItemsService
+import com.verge.theverge.services.TablesService
+import org.springframework.stereotype.Component
+
+@Component
+class PurchaseMapper(
+    private val itemsService: ItemsService,
+    private val tablesService: TablesService
+) {
+
+fun toModel(request: PostPurchaseRequest):PurchaseModel{
+    val table = tablesService.findTableById(request.tableId)
+    val items = itemsService.findAllById(request.itemsIds)
+
+    return PurchaseModel(
+        table = table,
+        items = items.toMutableList(),
+        price = items.sumOf { it.price
+        }
+    )
+}
+}
