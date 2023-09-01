13943d87ab64e68aa35c14989a138f2ef5ab846d
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-01 10:54:43 -0300

    Creation of purchase and purchase_item schemas.

---
+++ src/main/resources/db/migration/V11__create_table_purchase_and_purchase_item.sql
@@ -0,0 +1,17 @@
+CREATE TABLE purchase(
+purchase_id int auto_increment primary key,
+tables_id int not null,
+nfe varchar(255),
+price DECIMAL(15,2) not null,
+created_at datetime,
+payment_status boolean not null,
+FOREIGN KEY (tables_id) REFERENCES tables(tables_id)
+);
+
+CREATE TABLE purchase_item(
+purchase_id int not null,
+item_id int not null,
+FOREIGN KEY (purchase_id) REFERENCES purchase(purchase_id),
+FOREIGN KEY (item_id) REFERENCES items(item_id),
+PRIMARY KEY (purchase_id, item_id)
+)
