ffd27ed577ae917098c24426ab8c35c2df57c698
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-04 22:49:03 -0300

    Removed orderitems controller, service, repo, and model.

--- src/main/kotlin/com/verge/theverge/repository/OrderItemRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.verge.theverge.repository
-
-
-import com.verge.theverge.models.OrderItemModel
-import org.springframework.data.repository.CrudRepository
-
-interface OrderItemRepository:CrudRepository<OrderItemModel, Int> {
-
-}
