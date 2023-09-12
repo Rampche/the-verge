5aee6e4a9b851ef161864bbef857fa43a6958795
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-12 19:12:48 -0300

    Add async notation.

--- src/main/kotlin/com/verge/theverge/events/listener/GenerateNfeListenerFromOrder.kt
+++ src/main/kotlin/com/verge/theverge/events/listener/GenerateNfeListenerFromOrder.kt
@@ -3,6 +3,7 @@ package com.verge.theverge.events.listener
 import com.verge.theverge.events.OrderEvent
 import com.verge.theverge.services.OrderService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -10,6 +11,7 @@ import java.util.*
 class GenerateNfeListenerFromOrder(
     private val orderService: OrderService
 ) {
+    @Async
     @EventListener
     fun listen(orderEvent: OrderEvent){
         val nfe = UUID.randomUUID().toString()
