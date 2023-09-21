8eca7758b01b6db475dc03b1047cf9de9810aa17
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-09-21 18:43:29 -0300

    Updated to remove status and unused imports.

--- src/main/kotlin/com/verge/theverge/controller/requests/employee/PostEmployeeRequest.kt
+++ src/main/kotlin/com/verge/theverge/controller/requests/employee/PostEmployeeRequest.kt
@@ -1,11 +1,10 @@
 package com.verge.theverge.controller.requests.employee
 
-import com.verge.theverge.enums.EmployeeStatus
-import com.verge.theverge.enums.RoleType
-
 data class PostEmployeeRequest (
     val name: String,
+
     val email: String,
+
     val password: String,
-    val status: EmployeeStatus
+
 )
